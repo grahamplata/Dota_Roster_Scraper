b6800f0164dc119905e803039ca92a381ef34a7c
Graham Plata <graham.plata@gmail.com>
2016-09-01 17:57:39 -0400

    progress

--- webscraper.rb
+++ webscraper.rb
@@ -1,8 +1,39 @@
 require 'mechanize'
+require 'json'
 
-mechanize = Mechanize.new
+#setup mechanize
+mechanize = Mechanize.new { |agent|
+  agent.user_agent_alias = 'Mac Safari'
+}
 
+#content array
+contents = []
+#declare page to be scraped
 page = mechanize.get('http://www.dota2.com/majorsregistration/list')
 
+#find page title
 puts page.title
-puts
+
+#find table
+table = page.css('div.ConfirmationRow')
+#find row
+row = table.css('div.OptimizeTextRadiance')
+
+row.css('span').each_with_index do |x, index|
+  #team or player logic
+    if (index % 2 == 0)
+      player_name = x['player']
+      puts "\nPlayer: #{x.text}"
+    else
+      player_team = x['team']
+      puts "Team: #{x.text}"
+    end
+
+    contents.push(
+      player: player_name,
+      team: player_team
+      )
+
+end
+#generate JSON data
+  puts JSON.pretty_generate(contents)
