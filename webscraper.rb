c964a1cbb258657d162c17342b2a081f3adde10c
Eric Abruzzese <eric.abruzzese@gmail.com>
2016-09-02 17:26:00 -0400

    Refactor to use block syntax

--- webscraper.rb
+++ webscraper.rb
@@ -5,17 +5,27 @@ require 'json'
 
 TARGET_URL = 'http://www.dota2.com/majorsregistration/list'
 USER_AGENT = 'Mac Safari'
+SCRAPER = Mechanize.new { |agent| agent.user_agent_alias = USER_AGENT }
 
-mechanize = Mechanize.new { |agent| agent.user_agent_alias = USER_AGENT }
+# Scrapes a table-like element, and processes each row with a block
+def scrape_table(url, rows_selector, &block)
+  page = SCRAPER.get(url)
+  rows = page.css(rows_selector)
 
-page = mechanize.get(TARGET_URL)
+  contents = rows.map do |row|
+    yield(row)
+  end
 
+  contents
+end
+
+# Extracts text from the first member of a given CSS selector
 def extract_text(element, selector)
   element.css(selector).first.text.strip
 end
 
-rows = page.css('div.ConfirmationRow')
-contents = rows.map do |row|
+# Scrapes the table and processes each row
+output = scrape_table(TARGET_URL, 'div.ConfirmationRow') do |row|
   {
     player_name: extract_text(row, 'div.OptimizeTextRadiance:nth-child(4) > span'),
     team_name: extract_text(row, 'div.OptimizeTextRadiance:nth-child(5) > span'),
@@ -23,4 +33,4 @@ contents = rows.map do |row|
   }
 end
 
-puts JSON.pretty_generate(contents)
+puts JSON.pretty_generate(output)
