7be7c5db2bd93f08ec5282b8cd03941abff9bf93
Eric Abruzzese <eric.abruzzese@gmail.com>
2016-09-02 17:00:24 -0400

    Refactor to be more concise

--- webscraper.rb
+++ webscraper.rb
@@ -1,45 +1,26 @@
+#!/usr/bin/env ruby
+
 require 'mechanize'
 require 'json'
 
-#setup mechanize
-mechanize = Mechanize.new { |agent|
-  agent.user_agent_alias = 'Mac Safari'
-}
+TARGET_URL = 'http://www.dota2.com/majorsregistration/list'
+USER_AGENT = 'Mac Safari'
 
+mechanize = Mechanize.new { |agent| agent.user_agent_alias = USER_AGENT }
 
-#declare page to be scraped
-page = mechanize.get('http://www.dota2.com/majorsregistration/list')
+page = mechanize.get(TARGET_URL)
 
-#find page title
-puts page.title
-
-#find table
-table = page.css('div.ConfirmationRow')
-#find row
-row = table.css('div.OptimizeTextRadiance')
-
-
-#content arrays
-playerARRAY = []
-row.css('span').each_with_index do |x, index|
-  #scrape player logic
-  if index.even?
-    player_name = x.text
-    playerARRAY.push(player: player_name)
-  end
-end
-teamARRAY = []
-row.css('span').each_with_index do |x, index|
-  #scrape team logic
-  if index.odd?
-    team_name = x.text
-    teamARRAY.push(team: team_name)
-  end
+def extract_text(element, selector)
+  element.css(selector).first.text.strip
 end
 
-####################################################
-myhash = {}
-playerARRAY.each_with_index {|k,i|myhash[k] = teamARRAY[i]}
+rows = page.css('div.ConfirmationRow')
+contents = rows.map do |row|
+  {
+    player_name: extract_text(row, 'div.OptimizeTextRadiance:nth-child(4) > span'),
+    team_name: extract_text(row, 'div.OptimizeTextRadiance:nth-child(5) > span'),
+    action: extract_text(row, 'div.OptimizeTextRadiance:nth-child(6)')
+  }
+end
 
-
-puts JSON.pretty_generate(myhash)
+puts JSON.pretty_generate(contents)
