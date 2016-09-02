12530ba1984319e40d4c875f6fc46773d0e4084f
Eric Abruzzese <eric.abruzzese@gmail.com>
2016-09-02 18:20:52 -0400

    Refactored to be object-oriented.

--- webscraper.rb
+++ webscraper.rb
@@ -3,11 +3,64 @@
 require 'mechanize'
 require 'json'
 
-TARGET_URL = 'http://www.dota2.com/majorsregistration/list'
-USER_AGENT = 'Mac Safari'
-SCRAPER = Mechanize.new { |agent| agent.user_agent_alias = USER_AGENT }
+# Provides helper methods for interacting with Mechanize DOM elements
+module MechanizeHelper
+  # Extracts text from the first member of a given CSS selector
+  def extract_text(element, selector)
+    element.css(selector).first.text.strip
+  end
+end
 
-CONFIG = OpenStruct.new(
+# Scrapes a table-like element from a given URL, and allows the user to process
+# each row in the table with a provided block.
+class TableScraper
+  include MechanizeHelper
+
+  DEFAULT_USER_AGENT = 'Mac Safari'
+
+  def initialize(config)
+    @config = OpenStruct.new(config)
+    @scraper = Mechanize.new do |agent|
+      agent.user_agent_alias = @config.user_agent || DEFAULT_USER_AGENT
+    end
+  end
+
+  # Scrapes a table-like element, and processes each row with a block
+  def scrape(&block)
+    page = @scraper.get(@config.target_url)
+    rows = page.css(@config.rows_selector)
+
+    content = rows.map do |row|
+      yield(row)
+    end
+
+    JSON.pretty_generate(content)
+  end
+end
+
+# Scrapes a DotA roster from dota2.com
+class DotaRosterScraper < TableScraper
+  def scrape
+    super do |row|
+      if block_given?
+        yield(row, @config)
+      else
+        {
+          player_name: extract_text(row, @config.player_name_selector),
+          team_name: extract_text(row, @config.team_name_selector),
+          team_id: extract_text(row, @config.team_id_selector).match(@config.team_id_pattern).captures.first,
+          action: extract_text(row, @config.action_selector)
+        }
+      end
+    end
+  end
+end
+
+scraper = DotaRosterScraper.new(
+  # Scraper configuration
+  target_url: 'http://www.dota2.com/majorsregistration/list',
+  user_agent: 'Mac Safari',
+
   # Selectors
   rows_selector: 'div.ConfirmationRow',
   player_name_selector: 'div.OptimizeTextRadiance:nth-child(4) > span',
@@ -19,31 +72,4 @@ CONFIG = OpenStruct.new(
   team_id_pattern: /\(ID: (\d+)\)/i
 )
 
-# Scrapes a table-like element, and processes each row with a block
-def scrape_table(url, rows_selector, &block)
-  page = SCRAPER.get(url)
-  rows = page.css(rows_selector)
-
-  contents = rows.map do |row|
-    yield(row)
-  end
-
-  contents
-end
-
-# Extracts text from the first member of a given CSS selector
-def extract_text(element, selector)
-  element.css(selector).first.text.strip
-end
-
-# Scrapes the table and processes each row
-output = scrape_table(TARGET_URL, CONFIG.rows_selector) do |row|
-  {
-    player_name: extract_text(row, CONFIG.player_name_selector),
-    team_name: extract_text(row, CONFIG.team_name_selector),
-    team_id: extract_text(row, CONFIG.team_id_selector).match(CONFIG.team_id_pattern).captures.first,
-    action: extract_text(row, CONFIG.action_selector)
-  }
-end
-
-puts JSON.pretty_generate(output)
+puts scraper.scrape
